67f4a0621ddbf01667981d2f1ef385212aa0df2f
Krystian <dsw54152@student.dsw.edu.pl>
2024-06-09 22:52:43 +0200

    Dodanie pliku z komendami

---
+++ learningitbranching.sh
@@ -0,0 +1,17 @@
+git commit
+git checkout -b bugFix
+git merge bugFix;main
+git rebase main
+git checkout C4
+git checkout C4^
+git branch -f (nazwa)
+git reset
+git revert
+git cherry-pick
+git reabse -i
+git tag
+git clone
+git fetch
+git pull
+git fakeTeamwork
+git push
